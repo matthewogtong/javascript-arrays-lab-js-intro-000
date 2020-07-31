3866669c0358b52865b234eade538bb0a14c955c
Matthew Ogtong <ogtongm@gmail.com>
2020-07-31 00:57:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function destructivelyRemoveFirstKitten(name) {
 }
 
 function appendKitten(name) {
-  modifiedKittens = kittens.concat(name)
+  modifiedKittens = kittens.push(name)
   return modifiedKittens
 }
 
